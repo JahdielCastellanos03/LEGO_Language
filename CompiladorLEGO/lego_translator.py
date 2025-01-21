a3a681eb19b5d2f80ce100fad29b44633e10d566
JahdielCastellanos03 <fadeamsc@gmail.com>
2025-01-21 05:35:44 +0000

    Prueba

--- CompiladorLEGO/lego_translator.py
+++ CompiladorLEGO/lego_translator.py
@@ -4,7 +4,7 @@ from LEGOLexer import LEGOLexer
 from LegoToPython import LegoToPython
 
 def main():
-    input_file = "ejemplo.toycode"
+    input_file = "ejemplo.toycode -.---- debe jhalar"
     output_file = "output.py"
 
     try:
